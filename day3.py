c26a54cd5a344777842e4a99fb46bc4c3e75e235
Isaac Azuelos <isaac@azuelos.ca>
2025-12-03 20:15:40 -0700

    Day 3!

--- day3.py
+++ day3.py
@@ -17,40 +17,34 @@ TEST = parse(open("test/3.txt", "r").readlines())
 INPUT = parse(open("input/3.txt", "r").readlines())
 
 
-def max_joltage_2(bank):
-    first = max(bank[:-1])  # we can't use the last digit twice
-    first_i = bank.index(first)
-    second = max(bank[first_i + 1 :])
-
-    return int(first * 10 + second)
-
-
 def max_joltage(bank, n=2):
 
-    def inner(bank, n, digits=[]):
+    def inner(b, n, digits):
         if n == 1:
-            return max(bank)
+            digits.append(max(b))
+            return digits
 
-        d = max(bank[: -(n - 1)])  # we must leave digits
-        i = bank.index(first)
+        d = max(b[: -n + 1])  # we must leave digits
+        i = b.index(d)
 
-    digits = []
-    first = max(bank[: -(n - 1)])
+        digits.append(d)
+        return inner(b[i + 1 :], n - 1, digits)
 
-    product = 1
+    digits = inner(bank, n, [])
+
+    product = 0
     for p, d in enumerate(reversed(digits)):
         product += d * (10**p)
-
     return product
 
 
 def part1(input):
-    return sum((max_joltage(b) for b in input))
+    return sum(max_joltage(b) for b in input)
 
 
 def part2(input):
-    return [max_joltage(b, n=12) for b in input]
+    return sum(max_joltage(b, 12) for b in input)
 
 
 print("part 1:", part1(INPUT))
-print("part 2:", part2(TEST))
+print("part 2:", part2(INPUT))
